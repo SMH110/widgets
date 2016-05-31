e2b745eda4424be5fc0ba4cbc828d0aa15643925
SMH110 <mahdihassan135@hotmail.com>
2016-05-31 13:07:44 +0100

    2 calculators on the same page

--- javaScript.js
+++ javaScript.js
@@ -130,7 +130,155 @@ function Calculator() {
         this.input(newInput);
 
     }
+    
+    
+    
+    
+    /////////////////////////////////////
+    
+     let newInputRight = "0";
+    let oldInputRight = "";
+    let resultRight = "";
+
+    this.inputRight = ko.observable(newInputRight);
+    let operationRight = "";
+    let historyRight = [];
+
+    //-------------------------------------------------  ADD NUMBER -----------------------
+    this.addNumberRight = (number) => () => {
+        if (newInputRight === "0") {
+            newInputRight = number.toString();
+            historyRight.push(newInputRight)
+            this.inputRight(newInputRight);
+            return;
+        }
+        if (historyRight[historyRight.length - 1] === "=") {
+            newInputRight = "";
+        }
+        newInputRight += number.toString();
+        historyRight.push(newInputRight)
+        this.inputRight(newInputRight)
+    }
+
+    this.addZeroRight = () => {
+        if (newInputRight === "0") return;
+        if (historyRight[historyRight.length - 1] === "=") {
+            newInputRight = "";
+        }
+        newInputRight += "0";
+        historyRight.push(newInputRight)
+        this.inputRight(newInputRight);
+    }
+    
+    ///////////////////////////
+    //---------------------------------------------------------  OPERATIONS  -------------------
+    this.addPlusRight = () => {
+        if (historyRight.indexOf("+") > -1 || historyRight.indexOf("-") > -1 || historyRight.indexOf("*") > -1 || historyRight.indexOf("/") > -1) {
+            this.equalRight()
+        }
+        operationRight = "+";
+        if ( historyRight[historyRight.length - 1] === "-" || historyRight[historyRight.length - 1] === "+" || historyRight[historyRight.length - 1] === "*" || historyRight[historyRight.length - 1] === "/" ) return;
+
+        oldInputRight = newInputRight;
+        historyRight.push("+")
+        newInputRight = "";
+
+
+    }
+
+    this.addMinusRight = () => {
+        if (historyRight.indexOf("+") > -1 || historyRight.indexOf("-") > -1 || historyRight.indexOf("*") > -1 || historyRight.indexOf("/") > -1) {
+            this.equalRight()
+        }
+        operationRight = "-";
+        if ( historyRight[historyRight.length - 1] === "-" || historyRight[historyRight.length - 1] === "+" || historyRight[historyRight.length - 1] === "*" || historyRight[historyRight.length - 1] === "/" ) return;
+
+        oldInputRight = newInputRight;
+        historyRight.push("-")
+        newInputRight = "";
+
+    }
+
+    this.addMultiplicationRight = () => {
+        if (historyRight.indexOf("+") > -1 || historyRight.indexOf("-") > -1 || historyRight.indexOf("*") > -1 || historyRight.indexOf("/") > -1) {
+            this.equalRight()
+        }
+        operationRight = "*";
+        if ( historyRight[historyRight.length - 1] === "-" || historyRight[historyRight.length - 1] === "+" || historyRight[historyRight.length - 1] === "*" || historyRight[historyRight.length - 1] === "/" ) return;
+
+        oldInputRight = newInputRight;
+        historyRight.push("*")
+        newInputRight = "";
+
+    }
+
+    this.addDivisionRight = () => {
+         if (historyRight.indexOf("+") > -1 || historyRight.indexOf("-") > -1 || historyRight.indexOf("*") > -1 || historyRight.indexOf("/") > -1) {
+            this.equalRight()
+        }
+        operationRight = "/";
+        if ( historyRight[historyRight.length - 1] === "-" || historyRight[historyRight.length - 1] === "+" || historyRight[historyRight.length - 1] === "*" || historyRight[historyRight.length - 1] === "/" ) return;
+
+        oldInputRight = newInputRight;
+        historyRight.push("/")
+        newInputRight = "";
+
+    }
+
+
+    //-------------------------------------------------------EQUAL---------
+
+    this.equalRight = () => {
+        if (isNaN(historyRight[historyRight.length - 1])) return
+        if (operationRight === "" || oldInputRight === "") return;
+        if (operationRight === "+") {
+            resultRight = +oldInputRight + +newInputRight;
+        } else if (operationRight === "-") {
+            resultRight = +oldInputRight - +newInputRight;
+        } else if (operationRight === "*") {
+            resultRight = +oldInputRight * +newInputRight;
+        }else if (operationRight === "/") {
+            resultRight = +oldInputRight / +newInputRight;
+        }
+        
+        if (resultRight == Number.POSITIVE_INFINITY || resultRight == Number.NEGATIVE_INFINITY) {
+            resultRight = "0";
+        }
+
+        this.inputRight(resultRight);
+        historyRight = [];
+        historyRight.push(resultRight, "=")
+        newInputRight = resultRight;
+
+
+       
+
+
+    }
+
+
+    //-------------------------------------------------------CLEAR---------
+
+    this.clearRight = () => {
+        newInputRight = "0";
+        oldInputRight = "";
+        resultRight = "";
+        historyRight = [];
+        this.inputRight(newInputRight);
+
+    }
 
 }
+
+ko.applyBindings(new Calculator());
+/////////////////////////////////
+/////////////////////////////////
+
+
+
+////////////////////////////////
+
+
+
 
 ko.applyBindings(new Calculator());
