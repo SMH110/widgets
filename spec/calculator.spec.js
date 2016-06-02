ea1ecfb680c58d2bd31b8c85334b2eb6e44d0559
Massimo Ahmadi <massimo.ahmadi@adp.com>
2016-06-02 15:55:13 +0100

    Add HTML test runner

--- spec/calculator.spec.js
+++ spec/calculator.spec.js
@@ -1,24 +1,31 @@
-'use strict';
+(function (factory) {
+  if (typeof require === 'function' && typeof exports === 'object' && typeof module === 'object') {
+    module.exports = factory(require('chai'), require('../Calculator'));
+  } else {
+    factory(chai, Calculator);
+  }
+} (function (chai, Calculator) {
+  'use strict';
 
-const Calculator = require('../calculator');
-const expect = require('chai').expect;
+  const expect = chai.expect;
 
-describe('Calculator', () => {
-  let instance;
+  describe('Calculator', () => {
+    let instance;
 
-  beforeEach(() => {
-    instance = new Calculator();
+    beforeEach(() => {
+      instance = new Calculator();
+    });
+
+    it('It should report 0 when created', () => {
+      expect(instance.input()).to.equal('0');
+    });
+
+    it('It should be able to perform a simple add operation', () => {
+      instance.addNumber(3)();
+      instance.addPlus();
+      instance.addNumber(2)();
+      instance.equal();
+      expect(instance.input()).to.equal(5);
+    });
   });
-
-  it('It should report 0 when created', () => {
-    expect(instance.input()).to.equal('0');
-  });
-
-  it('It should be able to perform a simple add operation', () => {
-    instance.addNumber(3)();
-    instance.addPlus();
-    instance.addNumber(2)();
-    instance.equal();
-    expect(instance.input()).to.equal(5);
-  });
-});
+}));
