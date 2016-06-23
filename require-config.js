a59db08dfde91ec43fadf1d3000ff1fb8d38cf6f
SMH110 <mahdihassan135@hotmail.com>
2016-06-23 17:02:25 +0100

    Make To-Do-List

--- require-config.js
+++ require-config.js
@@ -2,6 +2,8 @@ require.config({
   deps: ['lib/app'],
   paths: {
     'chai': 'node_modules/chai/chai',
+    'sinon': 'node_modules/sinon/pkg/sinon',
+    'sinon-chai': 'node_modules/sinon-chai/lib/sinon-chai',
     'knockout': 'node_modules/knockout/build/output/knockout-latest'
   }
 });
