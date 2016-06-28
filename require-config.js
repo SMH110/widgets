904e24c572b87b64a7b2e04078f4deb8aef43a0b
massimocode <info@massimoahmadi.com>
2016-06-28 06:00:32 +0430

    Convert calculator to a knockout component

--- require-config.js
+++ require-config.js
@@ -4,6 +4,7 @@ require.config({
     'chai': 'node_modules/chai/chai',
     'sinon': 'node_modules/sinon/pkg/sinon',
     'sinon-chai': 'node_modules/sinon-chai/lib/sinon-chai',
-    'knockout': 'node_modules/knockout/build/output/knockout-latest'
+    'knockout': 'node_modules/knockout/build/output/knockout-latest',
+    'text': 'node_modules/requirejs-text/text'
   }
 });
