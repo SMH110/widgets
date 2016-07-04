e9fd59de34c7f3e8079e0c5b46cd92818374ed11
SMH110 <mahdihassan135@hotmail.com>
2016-07-05 00:47:54 +0100

    Change review to Weather Widget

---
+++ spec/weather-app.spec.js
@@ -0,0 +1,30 @@
+(function (factory) {
+  if (typeof define === 'function' && define['amd']) {
+    define(['chai', '../lib/weatherApp'], factory);
+  } else if (typeof require === 'function' && typeof exports === 'object' && typeof module === 'object') {
+    factory(require('chai'), require('../lib/weatherApp'));
+  } else {
+    factory(chai, sinon, ToDoList);
+  }
+} (function (chai, WeatherApp) {
+  'use strict';
+  const expect = chai.expect;
+  describe('WeatherApp', () => {
+    let instance;
+
+    beforeEach(() => {
+      instance = new WeatherApp();
+    });
+
+    it('It should start with empty input', () => {
+      expect(instance.city()).to.equal("");
+    });
+
+    it('It should disply the city which user has entered', () => {
+      instance.city("London");
+      // instance.showWeather(); // I don't know how to write test for fetch!
+      expect(instance.city()).to.equal("London");
+    });
+  });
+
+}));
